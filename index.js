adc35215e3b31d75a4bc818551d6a5d723e7752f
shakhawath <m.shakhawath18@gmail.com>
2022-05-09 10:17:14 +0600

    set all products api

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connect with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@user1.arezg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('CarZone').collection('cars');
+
+        // get all products
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
+    }
+    finally { };
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('hi')
+});
+
+app.listen(port, () => {
+    console.log('listening')
+})
