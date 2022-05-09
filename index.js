92a037d5aca6b393d165613fab8ebe57dda3100c
shakhawath <m.shakhawath18@gmail.com>
2022-05-09 23:29:23 +0600

    apply get one method

--- index.js
+++ index.js
@@ -16,16 +16,29 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const productCollection = client.db('CarZone').collection('cars');
+        const carCollection = client.db('CarZone').collection('cars');
 
         // get all products
-        app.get('/product', async (req, res) => {
+        app.get('/cars', async (req, res) => {
             const query = {};
-            const cursor = productCollection.find(query);
+            const cursor = carCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         });
-
+        // get one product
+        app.get('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const car = await carCollection.findOne(query);
+            res.send(car);
+        });
+        // app.get('/service/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id);
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service);
+        // });
 
     }
     finally { };
