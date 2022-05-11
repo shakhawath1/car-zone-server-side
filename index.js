16a10ba0a6974812c80568a64d7406b12772e58b
shakhawath <m.shakhawath18@gmail.com>
2022-05-11 12:22:08 +0600

    small correction

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function run() {
             const authHeader = req.headers.authorization;
             const email = req.query.email;
             const query = { email: email };
-            const cursor = orderCollection.find(query);
+            const cursor = carCollection.find(query);
             const car = await cursor.toArray();
             res.send(car);
 
